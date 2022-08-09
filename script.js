4ee848a3341391c4a118f4dfa096ba5110cba2ff
Emblogs <Emzy45cool@gmail.com>
2022-08-09 12:03:53 -0700

    MODDDY

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 let lastvalue="O"
 let display=document.getElementById("display")
-display.textContent="Player 1's turnto play"
-let boxes=document.querySelector(".box")
+display.textContent="Player 1's turn to play"
+let boxes=document.querySelectorAll(".box")
 
 for(let box of boxes) {
     box.addEventListener("click",() => {
         if(box.textContent !=="") {
             return;
         }
+        if (lastvalue =="O") {
+            box.textContent="X"
+            lastvalue="X"
+            display.textContent="Player 2's turn to play"
+            // checkGameStatus();
+            // checkDraw();
+        }
+        else{
+            box.textContent="O"
+            lastvalue="O"
+            display.textContent="Player 1's turn to play"
+            // checkGameStatus();
+            // checkDraw();
+        }
 })
 }
 
-if (lastvalue =="O") {
-    box.textContent="X"
-    lastvalue="X"
-    display.textContent="Player 2's turn to play"
-}
-else{
-    box.textContent="O"
-    lastvalue="X"
-}
+let ControlBtn=document.getElementById("ControlBtn")
+ControlBtn.addEventListener("click",() =>{
+    window.location.href="index.html"
+})
+
