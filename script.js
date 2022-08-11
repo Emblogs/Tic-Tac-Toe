09cf9ef033c897b5a7095e69937abf1cbd7db5cc
Emblogs <Emzy45cool@gmail.com>
2022-08-11 12:31:06 -0700

    Recently Added

--- script.js
+++ script.js
@@ -30,5 +30,47 @@ ControlBtn.addEventListener("click",() =>{
     window.location.href="index.html"
 })
 
+function checker(first,second,third) {
+    if(
+        boxes[first].textContent =="X" &&
+        boxes[second].textContent =="X" &&
+        boxes[third].textContent =="X" 
+    ) {
+        disableButtonsWhenGameIsWon("Player1")
+    } else if(
+        boxes[first].textContent =="O" &&
+        boxes[Second].textContent =="O" &&
+        boxes[Third].textContent =="O" 
+    ) {
+        disableButtonsWhenGameIsWon("Player2") 
+    }
+}
 
+function checkGameStatus() {
+    checker(0,1,2)
+    checker(3,4,5)
+    checker(6,7,8)
+    checker(0,3,6)
+    checker(1,4,7)
+    checker(2,5,8)
+    checker(0,4,8)
+    checker(2,4,6)
+}
 
+function disableButtonsWhenGameIsWon(player) {
+    for(let box of boxes) {
+        if (box.textContent =="") {
+            box.toggleAttribute("disabled")
+        }
+    }
+display.textContent=player+" won the game"
+}
+
+function checkDraw() {
+    for(let box of boxes) {
+        if (box.textContent ==""){
+            return;
+        }
+    }
+    display.textContent="Draw";
+}
